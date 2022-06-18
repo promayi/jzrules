1e55552712c634d08781ba3c27efe2535d7559d5
promayi <promayi@aliyun.com>
2022-06-18 12:34:01 +0800

    Update Airports.js

--- Surge/JS/Airports.js
+++ Surge/JS/Airports.js
@@ -24,7 +24,7 @@
   minutes = minutes > 9 ? minutes : "0" + minutes;
 
   $done({
-    title: `𝗧𝗲𝘅𝗼𝗻 | 𝘙𝘦𝘴𝘦𝘵 : ${resetDayLeft} days.`,
+    title: `𝗧𝗲𝘅𝗼𝗻 | 𝘙𝘦𝘴𝘦𝘵 : ${resetDayLeft} days. | 𝘌𝘹𝘱𝘪𝘳𝘦 : ${expire}`,
     content: content.join("\n"),
   });
 })();
