d51bab200f1a66ab665d463acc5f668404c5d54f
promayi <promayi@aliyun.com>
2022-06-18 20:43:15 +0800

    Update functionstatus.js

--- Surge/JS/functionstatus.js
+++ Surge/JS/functionstatus.js
@@ -17,7 +17,7 @@ if ($trigger == "button") {
 };
 $done({
     title:"Surge  Running"+startTime,
-    content:"Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
+    //content:"Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
     //icon: icon_s?"checkmark.seal":"exclamationmark.triangle",
    //"icon-color":icon_s?"#16A951":"#FF7500"
 });
