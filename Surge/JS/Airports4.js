8fcc58483f47de6711034ced459f27d46d46e781
promayi <promayi@aliyun.com>
2022-06-18 23:54:13 +0800

    Update Airports4.js

--- Surge/JS/Airports4.js
+++ Surge/JS/Airports4.js
@@ -26,7 +26,7 @@
 
   $done({
     //title: `𝗗𝗹𝗲𝗿 | 𝘙𝘦𝘴𝘦𝘵 : ${resetDayLeft} d | 𝘌𝘹𝘱 : 2023/3/14`,
-    title: `𝗗𝗹𝗲𝗿 | ${usedsizeGB} GB | ${totalsizeGB} GB`,
+    title: `𝗗𝗹𝗲𝗿 | ${usedsizeGB} GB | ${totalsizeGB} GB | ${toPercent(proportion)}`,
     content: content.join("\n"),
   });
 })();
