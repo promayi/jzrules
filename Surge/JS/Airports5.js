68ece5073312312da8a24ee2a8e8d28793fb96c0
promayi <promayi@aliyun.com>
2022-06-19 00:02:03 +0800

    Update Airports5.js

--- Surge/JS/Airports5.js
+++ Surge/JS/Airports5.js
@@ -11,7 +11,7 @@
   let usedsize = used / 1073741824;
   let usedsizeGB = usedsize.toFixed(2)
   let totalsize = total / 1073741824;
-  let totalsizeGB = totalsize.toFixed(2)
+  let totalsizeGB = totalsize.toFixed(0)
   /*
   let content = [`Used: ${toPercent(proportion)}, ${usedsizeGB} GB, Total: ${totalsizeGB} GB`];
   */
