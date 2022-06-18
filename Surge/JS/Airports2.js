ada13f72113c79d66a64c7a66d9d9f4bfff4cd48
promayi <promayi@aliyun.com>
2022-06-18 23:53:49 +0800

    Update Airports2.js

--- Surge/JS/Airports2.js
+++ Surge/JS/Airports2.js
@@ -26,7 +26,7 @@
 
   $done({
     //title: `𝗧𝗲𝘅𝗼𝗻 | 𝘙𝘦𝘴𝘦𝘵 : ${resetDayLeft} d | 𝘌𝘹𝘱 : 2022/9/6`,
-    title: `𝗧𝗲𝘅𝗼𝗻 | ${usedsizeGB} GB | ${totalsizeGB} GB`,
+    title: `𝗧𝗲𝘅𝗼𝗻 | ${usedsizeGB} GB | ${totalsizeGB} GB | ${toPercent(proportion)}`,
     content: content.join("\n"),
   });
 })();
